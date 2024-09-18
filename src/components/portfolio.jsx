facf49d2150e7aac818b61c06b08028a72ec30c4
MayHyeyeonKim <hkim7963@gmail.com>
2024-09-18 00:01:16 -0700

    updated new project and current my work

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -5,12 +5,73 @@ import bookdo from "../images/bookdo.png";
 import pad from "../images/pad.png";
 import ylh from "../images/ylh.png";
 import hnm from "../images/hnm.png";
+import DAS from "../images/DAS_GFL.png"
+import X from "../images/Demo.gif"
 
 const Portfolio = memo(({ refs }) => {
   return (
     <section ref={refs} className="portfolio-section">
       <div className="section-title">PORTFOLIO</div>
       <div className="portfolio-content">
+
+        {/* games for love */}
+        <div className="portfolio-info-area">
+          <div className="portfolio-info">
+            <div className="picture-area">
+              <img
+                src={DAS}
+                alt="죄송합니다 이미지를 불러오지 못했습니다."
+              />
+            </div>
+            <div className="title">Digital Aid Seattle</div>
+            <div className="contents">
+            Currently work here
+              <br />
+              <br /> Games for Love is a Gaming Charity dedicated to easing suffering, saving lives, and creating sustainable futures for children. We help kids heal through play. Games for Love began with a dream to benefit sick kids, and allow everyday heroes in #Gaming and #Tech to help children through their gifts and talents. 
+             
+            </div>
+            <a
+              className="button"
+              href="https://github.com/digitalaidseattle/games-for-love"
+            >
+              github link
+            </a>
+            <a className="button" href="https://games-for-love.web.app/">
+              publish link
+            </a>
+            <a className="button" href="https://www.digitalaidseattle.org/">DAS</a>
+            <a className="button" href="https://www.digitalaidseattle.org/project/recjTnXhnU7hkRDDb">GFL</a>
+          </div>
+        </div>
+
+        {/* exploreX */}
+        <div className="portfolio-info-area">
+          <div className="portfolio-info">
+            <div className="picture-area">
+              <img
+                src={X}
+                alt="죄송합니다 이미지를 불러오지 못했습니다."
+              />
+            </div>
+            <div className="title">ExploreX</div>
+            <div className="contents">
+              Team Project 2024
+              <br />
+              <br /> 97.1K subscribers YouTube REACT Award Winner
+              <br /> ExploreX is a travel booking and activity management platform built using React. This project was developed by a team of five as a group project.
+              <br /> Built with React, React Query and deployed using Netlify.
+            </div>
+            <a
+              className="button"
+              href="https://github.com/ExploreX-App/ExploreX"
+            >
+              github link
+            </a>
+            <a className="button" href="https://explorex-9.netlify.app/">
+              publish link
+            </a>
+          </div>
+        </div>
 
         {/* bookdo7stars */}
         <div className="portfolio-info-area">
@@ -25,7 +86,7 @@ const Portfolio = memo(({ refs }) => {
             <div className="contents">
               Team Project 2024
               <br />
-              <br /> 95.3K subscribers YouTube Award Winner
+              <br /> 97.1K subscribers YouTube Node FullStack Award Winner
               <br /> Built with MongoDB, Express, React, Redux, and Node.js, and
               deployed using AWS Elastic Beanstalk.
             </div>
